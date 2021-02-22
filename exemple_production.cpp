66d0bfe2127d715b62a329dd4b9051fb398cf79d
Alexandre Chanson <a.chanson.tours@gmail.com>
2021-02-22 16:53:24 +0100

    fixed a lot of of by 1 error (damn indexes ...), added test code for combo

--- exemple_production.cpp
+++ exemple_production.cpp
@@ -1,16 +1,58 @@
 ﻿#include "instance.h"
 #include "solver.h"
+#include "combo.h"
 #include <iostream>
 #include <string>
+
+
+// combo will not compile with sdl check sue to several C4703 warnings when initializing stuff in switches
+// item : profit/wheight/keep
+bool* exclude_nodes(cplex_tap::Instance tap, int lb_interest) {
+	cout << "Filtering Nodes" << endl;
+	int maxDist = 0;
+	for (auto i = 0; i < tap.size(); ++i) {
+		for (auto j = i + 1; j < tap.size(); ++j) {
+			int d = tap.dist(i, j);
+			if (d > maxDist)
+				maxDist = d;
+		}
+	}
+	maxDist++;
+
+	// Debug for combo
+	item items[3];
+	items[0] = { 2, 2, false };
+	items[1] = { 5, 3, false };
+	items[2] = { 1, 5, false };
+
+	item* start = items;
+	item* end = (item*)(&items + 1) - 1;
+	// run combo (pointer to first item, pointer to last item, max whieght, starting lb, starting ub, set x in items, no relaxation)
+	stype s = combo(start, end, 6, 0, 99, true, false);
+
+	
+	bool out[3]; // out[tap.size()]
+	for (auto i = 0; i < 3; ++i) { //i < tap.size()
+		out[i] = items[i].x;
+		cout << out[i];
+	}
+	cout << endl;
+	return out;
+}
 
 int main() {
 	using namespace cplex_tap;
 
 	std::cout << "Loading TAP instance\n";
+	//const auto tap = Instance("C:\\Users\\achan\\source\\repos\\cplex_test\\instances\\tap_500.dat");
 	const auto tap = Instance("C:\\Users\\achan\\source\\repos\\cplex_test\\small_test_instance.txt");
+
+	exclude_nodes(tap, 5);
+
 	const auto solver = Solver(tap);
 
-	solver.solve_and_print();
+	solver.solve_and_print(5, 1000);
 
 	return 0;
 }
+
