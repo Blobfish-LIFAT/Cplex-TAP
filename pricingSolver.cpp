105546c2885a09f5d6e68135fa3a41c5ad4c5b8d
Alexandre Chanson <a.chanson.tours@gmail.com>
2023-02-02 16:53:09 +0100

    disabled all heuristics switch mip emphasis

--- pricingSolver.cpp
+++ pricingSolver.cpp
@@ -574,7 +574,7 @@ namespace cplex_tap {
             cplex_solver.setParam(IloCplex::Param::Preprocessing::Symmetry, 0);
             cplex_solver.setParam(IloCplex::Param::Preprocessing::Aggregator, 0);
             cplex_solver.setParam(IloCplex::Param::Preprocessing::Presolve	, 0);
-            cplex_solver.setParam(IloCplex::Param::MIP::Display, 5); //5 is max
+            cplex_solver.setParam(IloCplex::Param::MIP::Display, 0); //5 is max
             cplex_solver.setParam(IloCplex::Param::Simplex::Display, 0);
             cplex_solver.setParam(IloCplex::Param::MIP::Strategy::HeuristicEffort, 0);
             cplex_solver.setParam(IloCplex::Param::Emphasis::MIP	, 2);
