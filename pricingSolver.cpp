ba8061e7b9dc09f3f486c23830c7361845684081
Alexandre Chanson <a.chanson.tours@gmail.com>
2023-06-21 17:00:41 +0200

    lp vs mip

--- pricingSolver.cpp
+++ pricingSolver.cpp
@@ -81,7 +81,7 @@ namespace cplex_tap {
         int it = 0;
         time_t start;
         start = clock();
-        while (it++ < 100) {
+        while (it++ < 150) {
 
             if (!NO_PRINT) std::cout << "[STEP] Building RMP model" << std::endl;
             Instance rmpIST = buildRMPInstance(rmpQSet);
